5712331b0f3ec7597fac311f4296e29e6540e21e
Jorge.Melero <xmelerikox@gmail.com>
2016-11-15 20:24:22 +0100

    a

--- Ejercicio16/src/Ejercicio16.java
+++ Ejercicio16/src/Ejercicio16.java
@@ -4,46 +4,36 @@ public class Ejercicio16 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a,b,c,x1,x2,divisor,dividendo1,dividendo2,raiz;	
+		float a,b,c,x1,x2,divisor,dividendo1,dividendo2,raiz;	
 		Scanner teclado= new Scanner(System.in);
 		System.out.println("Escribe el valor de a:");
-		a =teclado.nextInt();
+		a =teclado.nextFloat();
 		System.out.println("Escribe el valor de b:");
-		b =teclado.nextInt();
+		b =teclado.nextFloat();
 		System.out.println("Escribe el valor de c:");
-		c =teclado.nextInt();
+		c =teclado.nextFloat();
 		raiz=b*b-4*a*c;
 		if (raiz<0)
-		raiz=raiz-(raiz*2);
-		
-		dividendo1=(int) (-b+Math.sqrt(raiz));
-		dividendo2=(int) (-b-Math.sqrt(raiz));
+			raiz=raiz-(raiz*2);
+
+		dividendo1=(float) (-b+Math.sqrt(raiz));
+		dividendo2=(float) (-b-Math.sqrt(raiz));
 		divisor=(2*a);
 		x1=dividendo1/divisor;
 		x2=dividendo2/divisor;
 		raiz=b*b-4*a*c;
 		if (raiz<0)
-		{
-		if(dividendo1%divisor==0)
-			System.out.println("El valor de x1 es:"+x1);
-		else
-			System.out.println("El valor de x1 es:"+dividendo1+"/"+divisor);
-		if (dividendo2%divisor==0)
+		{	
+			System.out.println("El valor de x1 es:"+x1+"*√-1");
 			System.out.println("El valor de x2 es:"+x2+"*√-1");
-		else
-			System.out.println("El valor de x2 es:"+dividendo2+"*√-1/"+divisor);
 		}
 		else
 		{
-		if(dividendo1%divisor==0)
 			System.out.println("El valor de x1 es:"+x1);
-		else
-			System.out.println("El valor de x1 es:"+dividendo1+"/"+divisor);
-		if (dividendo2%divisor==0)
 			System.out.println("El valor de x2 es:"+x2);
-		else
-			System.out.println("El valor de x2 es:"+dividendo2+"/"+divisor);
 		}
+
+
 	}
 
 }
