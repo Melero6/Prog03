8f255188ed86faa2029101a18e57990900601881
Melero6 <xmelerikox@gmail.com>
2016-12-09 18:32:28 +0100

    Update Ejercicio09.java

--- Ejercicio09/src/Ejercicio09.java
+++ Ejercicio09/src/Ejercicio09.java
@@ -8,18 +8,17 @@ public class Ejercicio09 {
 		// TODO Auto-generated method stub
 		Scanner teclado= new Scanner(System.in);
 		int n, cont, resu=0;
+		
 		System.out.println("Escribe el valor de N:");
-		n= teclado.nextInt();
-		cont=n;
+		n = teclado.nextInt();
+		cont = n;
+		
 		System.out.print("El numero "+n);
 		while(cont!=0)
 		{
 			--cont;
 			if(cont!=0&&(n%cont)==0)
-			{
 				resu=resu+cont;
-			
-			}
 		}
 		if (resu==n)
 			System.out.print(" es perfecto.");
